7fe862d5a7e7e19348091c3df39ca04085448729
Dimitri1 <dimitri.gerin@gmail.com>
2013-07-24 01:09:35 +0200

    Adding updated libglosm-geomgen

---
+++ libglosm-geomgen/CMakeFiles/glosm-geomgen.dir/DependInfo.cmake
@@ -0,0 +1,15 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dimitri/Dropbox/trc_simulator_8/glosm/libglosm-geomgen/GeometryGenerator.cc" "/home/dimitri/Dropbox/trc_simulator_8/glosm/libglosm-geomgen/CMakeFiles/glosm-geomgen.dir/GeometryGenerator.cc.o"
+  "/home/dimitri/Dropbox/trc_simulator_8/glosm/libglosm-geomgen/MetricBasis.cc" "/home/dimitri/Dropbox/trc_simulator_8/glosm/libglosm-geomgen/CMakeFiles/glosm-geomgen.dir/MetricBasis.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/dimitri/Dropbox/trc_simulator_8/glosm/libglosm-server/CMakeFiles/glosm-server.dir/DependInfo.cmake"
+  )
---
+++ libglosm-geomgen/CMakeFiles/glosm-geomgen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/glosm-geomgen.dir/GeometryGenerator.cc.o"
+  "CMakeFiles/glosm-geomgen.dir/MetricBasis.cc.o"
+  "libglosm-geomgen.pdb"
+  "libglosm-geomgen.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/glosm-geomgen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
